980761d358168c8f23d6e691640de38dd0d4afd0
Nathan Pabst <Nathan.Pabst@ey.com>
2020-09-15 07:56:20 -0500

    creating initial project

---
+++ CsharpAdvanced/CsharpAdvanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
