4a7038753d37530ec57e6ca28ef8d3e0bb178979
Nathan Pabst <Nathan.Pabst@ey.com>
2020-09-16 07:58:30 -0500

    Generics section wip.

--- CsharpAdvanced/CsharpAdvanced/Booklist.cs
+++ CsharpAdvanced/CsharpAdvanced/Booklist.cs
@@ -4,6 +4,9 @@ namespace CsharpAdvanced
 {
     public class Booklist
     {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+
         public void Add(Book book)
         {
             throw new NotImplementedException();
@@ -15,13 +18,24 @@ namespace CsharpAdvanced
         }
     }
 
-    public class ObjectList
+    public class Book
     {
-        public void Add(object value)
+    }
+
+    public class GenericDictionary<TKey, TValue>
+    {
+        public void Add(TKey key, TValue value)
+        {
+        }
+    }
+
+    public class GenericList<T>
+    {
+        public void Add(T value)
         {
         }
 
-        public object this[int index]
+        public T this[int index]
         {
             get { throw new NotImplementedException(); }
         }
--- CsharpAdvanced/CsharpAdvanced/Program.cs
+++ CsharpAdvanced/CsharpAdvanced/Program.cs
@@ -6,7 +6,16 @@ namespace CsharpAdvanced
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var book = new Booklist() { Isbn = "1111", Title = "C# Advanced" };
+
+            var numbers = new GenericList<int>();
+            numbers.Add(10);
+
+            var books = new GenericList<Book>();
+            books.Add(new Book());
+
+            var dictionary = new GenericDictionary<string, Book>();
+            dictionary.Add("12345", new Book());
         }
     }
 }
