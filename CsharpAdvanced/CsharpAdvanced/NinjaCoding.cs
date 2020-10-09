a6e7a2dea64ba9605558f8b5635ce8de5b5c478a
Nathan Pabst <Nathan.Pabst@ey.com>
2020-10-09 07:49:24 -0500

    ninja coding text selection complete

---
+++ CsharpAdvanced/CsharpAdvanced/NinjaCoding.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpAdvanced
+{
+    internal class NinjaCoding
+    {
+        // Ninja Coding TEXT SELECTION
+        // Control + right OR left arrow: moves the cursor to the right or left one word at a time
+        // Function + home: moves the cursor to the beginning of the line
+        // Function + end: moves the cursor to the end of the current line
+        // fn + up arrow OR down arrow: scrolls to the next method
+        // control + function + end: jump to the end of the file
+        // control + function + home: jump to the beginning of the file
+        // shift key: to activate selection
+        // shift + function + right arrow: select from where the cursor is to the end of the line
+        // shift + function + left arrow: select from where the cursor is to the beginning of the line
+        // control + c: copies the current line
+        // control + v: pastes the line below
+        // control + x: removes the current line
+    }
+}
