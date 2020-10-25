ea9a16b58a30e0ea7cdcc792491fba579755f39e
Nathan Pabst <Nathan.Pabst@ey.com>
2020-10-25 08:26:03 -0500

    Sample SP in place. section 3 complete.

--- CsharpAdvanced/CsharpAdvanced/StoredProcedures.cs
+++ CsharpAdvanced/CsharpAdvanced/StoredProcedures.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace CsharpAdvanced
@@ -16,5 +17,26 @@ namespace CsharpAdvanced
         // 2. Encapsulate Biz Logic
         // 3. Stronger Security
         // 4. Efficiency
+        //_____Stored Procedure: Section 3___
+        // Objective...Calculate the time to travel a distance
+        // Steps...
+        // 1. accept distance and velocity as parameters
+        // 2. calculate time as distance / velocity
+        // 3. output the result
+        // Sample SP...
+        //CREATE PROCEDURE [dbo].upsCalculateTime
+        //// add the parameters here...
+        //@distance float,
+        //@velocity float
+        //AS
+        //BEGIN
+        //// set nocount on to prevent extra result sets from interfering with the select statements
+        //  SET NOCOUNT ON;
+        //// insert statements for procedure...
+        //      IF (@velocity <> 0.00)
+        //          SELECT @distance / @velocity;
+        //      ELSE
+        //          SELECT 0.00;
+        //END
     }
 }
