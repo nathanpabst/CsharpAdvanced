94fcc202d767be988e240f3d7af6348a4ba0f9f6
Nathan Pabst <Nathan.Pabst@ey.com>
2020-10-25 08:08:31 -0500

    section 2 complete.

---
+++ CsharpAdvanced/CsharpAdvanced/StoredProcedures.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpAdvanced
+{
+    internal class StoredProcedures
+    {
+        // ____What is a SP & why do we need them?_______
+        // Elements of a SP...
+        // inputs - name and parameters
+        // execution - select statement and parameter used as criteria
+        // output - the result set returned
+        // Why SPs? ...
+        // 1. Maintainability
+        // 2. Encapsulate Biz Logic
+        // 3. Stronger Security
+        // 4. Efficiency
+    }
+}
