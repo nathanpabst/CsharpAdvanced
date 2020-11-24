cf285fb86befbedc4ed946a07f54110379f6d616
Nathan Pabst <Nathan.Pabst@ey.com>
2020-11-24 08:03:45 -0600

    adding SQL queries for practice.

--- CsharpAdvanced/CsharpAdvanced/SQLstuff.cs
+++ CsharpAdvanced/CsharpAdvanced/SQLstuff.cs
@@ -10,7 +10,7 @@ namespace CsharpAdvanced
         //SELECT, INSERT, UPDATE, DELETE
         //__________Data Definition Language (DDL) Commands__________
         //CREATE DATABASE, ALTER DATABASE, DROP DATABASE, CREATE TABLE, ALTER TABLE, DROP TABLE
-        //__________Select from multiple tables______________
+        //__________SELECTING FROM MULTIPLE TABLES______________
         //separate each table with a comma, qualify any references to columns by placing the table name in front and separated by a dot
         //EX..
         // select * from Individual, Occupation
@@ -29,6 +29,7 @@ namespace CsharpAdvanced
         // where FirstName = 'Homer'
         //______________WHERE Clause______________
         // SQL WHERE SYNTAX: select * from table_name where column_name = 'criteria';
+        // EX...select * from[dbo].[User] u WHERE u.Active = 0 order by u.LastName desc;
         // EX...select * from Individual where FirstName = 'homer';
         //______________Multiple Conditions (AND and OR)____________
         // EX... select * from Individual
@@ -37,25 +38,28 @@ namespace CsharpAdvanced
         // EX...select * from Individual
         // where FirstName = 'Homer'
         // or LastName = 'Simpson';
-        //_____________Order by________________
-        // EX... select * from Individual
-        // order by LastName desc; --using the desc keyword will return the highest values first
+        //_____________ORDER BY________________
+        // EX...select * from[dbo].[User] u WHERE u.Active = 0 order by u.LastName desc;
+        // EX... select * from Individual order by LastName desc; --using the desc keyword will return the highest values first
         //_____________Sorting by Multiple Columns__________
         // you can sort by multiple columns by stating each column in the order by clause
         //...and separating each column name with a comma. SQL will first order the results by the first column, then second, third, etc.
-        // EX..select * from Individual
-        // order by LastName, FirstName;
+        // EX..select * from Individual order by LastName, FirstName;
+        // EX..select u.FirstName, u.LastName, u.RoleId from [dbo].[User] u
+        //...order by u.RoleId desc, u.LastName; --returns first name, last name and role id.primary sort = role id listed in descending order(4,3,2,1)
         //____________TOP_________________________
         // the 'top' clause is Transact-SQL and not part of the ANSI SQL...
         // ...depending on your db system, this clause may not be available
         // EX.. select top 3 * Individual;
+        // EX..select top 5 * from[dbo].[User] --returns the first five results (chronologically by Id)
         //____________PERCENT_________________________
         // EX.. select top 42 percent * Individual;
         // EX 2.. select top 42 percent * Individual order by LastName desc;
+        // EX..select top 10 percent u.UserName, u.LastName, u.Active, u.RoleId from [dbo].[User] u
         //____________DISTINCT________________________
         // the distinct keyword allows you to find unique values in a column
         // ..once a table starts getting a lot of data in it, some columns will contain duplicate values.
-        //..many indiduals share first names and surnames. to find out how many unique values there are in a column, you can use the distinct keyword
+        //..many individuals share first names and surnames. to find out how many unique values there are in a column, you can use the distinct keyword
         // EX..select distinct(FirstName) from Individual; NOTE: in this example, all customers with the same first name
         //..will be treated as one. this results in only one entry per first name
         //____________IN________________________
@@ -70,12 +74,8 @@ namespace CsharpAdvanced
         // NOTES: the 'in' operator is especially useful when you need to compare a value against the result of another query.
         //..example, lets say we have two tables (Individual and Publisher--which contains read/write permissions)...All users in the publisher table are also in the Individual table,
         //..but not all users in the Individual table are also in the Publisher table
-        // EX...
-        // select UserName from Individual
-        // where IndividualId in
-        // (select IndividualId
-        // from Publisher
-        // where AccessLevel = 'contributor);
+        // EX...select UserName from Individual where IndividualId in (select IndividualId from Publisher where AccessLevel = 'contributor);
+        // EX...select u.UserName from[dbo].[User] u where u.RoleId IN (select Id from Role where Name = 'viewer'); --returns the username of any user assigned the viewer role
         //______________SQL ALIAS___________________
         // an alias is a name that you give a table
         // use cases: when you need to reference the same table name over and over again
