cee0aba3023bd55d9f934538fa5c592ff1c06517
Nathan Pabst <Nathan.Pabst@ey.com>
2020-12-03 16:24:27 -0600

    Starting a new sql server course.

---
+++ CsharpAdvanced/CsharpAdvanced/SQLserver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpAdvanced
+{
+    internal class SQLserver
+    {
+        // WHAT: SQL Server is a relational database management system(RDBMS) developed by Microsoft.
+        // SQL Server is extremely versatile and it can be used at all ends of the spectrum — from storing the content for an individual's blog, to storing customer data and providing analytics for small to medium enterprises, to being an integral part of the enterprise systems of some of the world's largest companies.
+
+        // COMPETITORS: Some of SQL Server's competitors include Oracle, MySQL, PostgreSQL, etc.
+
+        // CLIENT/SERVER DATABASE SYSTEMS
+        // SQL Server is a client/server database management system(DBMS). This means that you can have many different "client" machines
+        // all connecting to SQL Server at the same time(or at different times). And each one of those client machines could be connecting
+        // via a different tool. SQL Server can also be managed from the server that it's installed on, but the main benefit of a client/server DBMS
+        // is that multiple users can access it simultaneously, each with a specific level of access.
+        // If the database administrators have done their job properly, any client that connects to SQL Server will only be able to access the
+        // databases that they're allowed to access. And they can only perform the tasks that they're allowed to perform. (controlled from within SQL Server
+
+        // SQL SERVER MANAGEMENT TOOLS: gui tools, azure data studio, ssms, dbeaver + several Command Line Tools
+
+        // CONNECTING TO SQL SERVER INSTRUCTIONS : https://www.quackit.com/sql_server/sql_server_2017/tutorial/create_a_database_in_sql_server_2017.cfm
+    }
+}
