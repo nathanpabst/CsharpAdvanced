e048be322aaae15a20255a5941953f30f1349b8d
Nathan Pabst <Nathan.Pabst@ey.com>
2020-12-04 08:03:39 -0600

    adding syntax and notes on the create table statement.

--- CsharpAdvanced/CsharpAdvanced/SQLserver.cs
+++ CsharpAdvanced/CsharpAdvanced/SQLserver.cs
@@ -23,6 +23,24 @@ namespace CsharpAdvanced
 
         // CONNECTING TO SQL SERVER INSTRUCTIONS : https://www.quackit.com/sql_server/sql_server_2017/tutorial/create_a_database_in_sql_server_2017.cfm
 
+        //_________EXPLANATION OF THE CREATE TABLE STATEMENT
+        //USE Music; --ensures that we are creating the tables within the correct db. i.e. merely precautionary
+        //CREATE TABLE Artists( --artists = the name of the table that is to be created
+        //    ArtistId int IDENTITY(1,1) NOT NULL PRIMARY KEY, --defining the column name (ArtistId) within the table, datatype (int), column type and how to increment the IDs (Identity(1,1)), nullable type, and defines the column as the PK for the table.
+        //    ArtistName nvarchar(255) NOT NULL, --defines the column name: ArtistName, datatype: nvarchar(255), and nullable type: not null
+        //    ActiveFrom date --defines the column name: ActiveFrom, datatype: date
+        //); --use ')' to close the definition & ';' to end the statement (the semi-colon is a statement terminator)
+        //GO --Signals the end of the batch of Transact-SQL statements
+        //
+        // Additional Notes:
+        // IDENTITY(1,1): sets the column as an identity column and provides the unique ID for the table and the value will increment with each row added
+        // (1,1) : means that the value starts at 1 and increments by 1
+        // NOT NULL : means that the field cannot contain null values
+        // PRIMARY KEY : sets the column as the primary key for the table. PK is a column that has been configured as the unique identifier for the table
+
+        // _________CREATING A RELATIONSHIP BETWEEN TWO TABLES
+        //
+        // ..
         // current location: https://www.quackit.com/sql_server/sql_server_2017/tutorial/create_a_table_in_sql_server_2017.cfm
 
         //https://www.quackit.com/sql_server/sql_server_2017/tutorial/
